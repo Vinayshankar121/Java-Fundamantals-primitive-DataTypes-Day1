f63ff23152f103878baf02a5b7821c25e2fc845e
Vinay Shankar <vinaynukala65@gmail.com>
2024-06-18 11:43:00 +0530

    feat:PrimitiveDataTypes.java

---
+++ PrimitiveDataTypes.java
@@ -0,0 +1,27 @@
+public class PrimitiveDataTypes {
+    public static void main(String[]args) {
+       
+        String empname;
+        empname = "aaa";
+         long empsalary;
+        salary = 10000;
+        int empid;
+        empid = 121;
+        byte empage;
+        empage = 20;
+        char empsec;
+        empsec = 'A';
+
+
+        System.out.println("employee name" + empname);
+        System.out.println("employee salary" + empsalary);
+        System.out.println("employee id" + empid);
+        System.out.println("employee age" + empage); 
+
+
+
+        
+
+    }
+
+}
